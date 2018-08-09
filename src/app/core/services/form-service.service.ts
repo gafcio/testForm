5a5203805915f30aef9adea3ab35ee0c0230beaa
Gawel <gawel.konefeld@mentax.pl>
2018-08-09 14:43:36 +0200

    rest files

--- src/app/core/services/form-service.service.ts
+++ src/app/core/services/form-service.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getEmployees ( {
+    return this.http.get('./src/assets/categories.json');
+  }
 }
